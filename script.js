957adc1c893c0d9672dd346174c5a6cade4b7694
Haafizz Ismail <mdnurhaafizz@gmail.com>
2022-02-22 22:19:12 +0800

    complete game

---
+++ script.js
@@ -0,0 +1,58 @@
+function computerPlay() {
+    const selection = ['Rock', 'Paper', 'Scissors'];
+    return selection[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'Rock') {
+        return "It's a Tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+        computerScore += 1;
+        return "You Lose! Paper beats Rock.";
+    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        playerScore += 1;
+        return "You Win! Rock beats Scissors.";
+    }
+
+    if (playerSelection === 'paper' && computerSelection === 'Paper') {
+        return "It's a Tie!";
+    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        computerScore += 1;
+        return "You Lose! Scissors beats Paper.";
+    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        playerScore += 1;
+        return "You Win! Paper beats Rock.";
+    }
+
+    if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
+        return "It's a Tie!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        computerScore += 1;
+        return "You Lose! Rock beats Scissors.";
+    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        playerScore += 1;
+        return "You Win! Scissors beats Paper.";
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection;
+        while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            playerSelection = prompt('Rock, Paper or Scissors? (Type Correctly)').toLowerCase();
+        }
+
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    console.log(`Player Score: ${playerScore}.`);
+    console.log(`Computer Score: ${computerScore}.`);
+    if (playerScore > computerScore) {
+        console.log('You are the winner!');
+    } else console.log('The Computer is the winner!');
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
